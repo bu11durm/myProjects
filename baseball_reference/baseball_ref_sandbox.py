0476a30b7488903535f1bcea2237a4b7cb64f3a4
bu11durm <bake@nc.rr.com>
2020-08-26 09:03:58 -0400

    commit test

--- baseball_reference/baseball_ref_sandbox.py
+++ baseball_reference/baseball_ref_sandbox.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.action_chains import ActionChains
 
+# adding comment for github test
 driver = webdriver.Chrome('../venv/chromedriver.exe')
 driver.get("https://www.baseball-reference.com/players/m/morgajo02.shtml")
 driver.set_window_size(700, 1000)
